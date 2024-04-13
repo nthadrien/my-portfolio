11606df9bd253c8a03891d8e4256201c31f16f04
nthadrien <91905798+nthadrien@users.noreply.github.com>
2024-04-13 15:02:02 -0700

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        primary:"var(--pry-clr)",
+        secondary:"var(--snd-clr)",
+        neutral:"var(--ntr-clr)",
+        "dark-primary":"var(--drk-pry-clr)",
+
+      }
+    },
+  },
+  plugins: [],
+}
